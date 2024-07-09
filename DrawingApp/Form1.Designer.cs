9308a2d228551474cdfab0a31d8161de22c941d5
Dzifa Oheneba <VinandoGH>
2024-07-09 21:51:49 +0000

    Drawing App

---
+++ DrawingApp/Form1.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.canvasPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.Location = new System.Drawing.Point(12, 12);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(500, 500);
+            this.canvasPanel.TabIndex = 0;
+            this.canvasPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseDown);
+            this.canvasPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseMove);
+            this.canvasPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(524, 524);
+            this.Controls.Add(this.canvasPanel);
+            this.Name = "Form1";
+            this.Text = "Drawing App";
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Panel canvasPanel;
+    }
+}
---
+++ DrawingApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point previousPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void canvasPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            previousPoint = e.Location;
+        }
+
+        private void canvasPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = canvasPanel.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, previousPoint, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+
+        private void canvasPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+    }
+}
